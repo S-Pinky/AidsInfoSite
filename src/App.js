af73092d7e63ff2ce030cccfb761b1c894b8c2cb
DESKTOP-RJJHN1M\samuel <sbernardes467@gmail.com>
2022-03-08 23:16:17 -0300

    creating website

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-
-import { Doctors } from "./pages/Doctors";
-import { Home } from "./pages/Home";
+import { Blog } from "./pages/Home/index"
+import Header from "./components/Header";
 
 const App = () => {
-  const [selectCity, setSelectCity] = useState("");
-  const [selectSpecialty, setSelectSpecialty] = useState("");
-  const [checkCrm, setCheckCrm] = useState("");
 
   return (
     <Router>
@@ -19,24 +14,7 @@ const App = () => {
         <Route
           path="/"
           element={
-            <Home
-              setSelectCity={setSelectCity}
-              setSelectSpecialty={setSelectSpecialty}
-              setCheckCrm={setCheckCrm}
-              selectCity={selectCity}
-              selectSpecialty={selectSpecialty}
-            />
-          }
-        />
-        <Route
-          path="/doctors"
-          element={
-            <Doctors
-              selectCity={selectCity}
-              selectSpecialty={selectSpecialty}
-              checkCrm={checkCrm}
-              setCheckCrm={setCheckCrm}
-            />
+            <Blog />
           }
         />
       </Routes>
